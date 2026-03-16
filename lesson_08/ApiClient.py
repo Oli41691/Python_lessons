51fcd89879d7a20dd39aca35c9191a0e9c0e88a5
oli41691 <oli41691@gmail.com>
2026-03-16 21:10:07 +0300

    lesson_08

--- lesson_08/ApiClient.py
+++ lesson_08/ApiClient.py
@@ -8,12 +8,14 @@ class ApiClient:
         self.login = login
         self.password = password
 
-
     def auth_data(self):
-        resp = requests.post(f"{self.base_url}/api-v2/auth/companies", json={
-        "login": self.login,
-        "password": self.password
-    })
+        resp = requests.post(
+            f"{self.base_url}/api-v2/auth/companies",
+            json={
+                "login": self.login,
+                "password": self.password
+                }
+            )
         resp.raise_for_status()
         company_id = resp.json()['content'][0]['id']
         return {
@@ -31,40 +33,37 @@ class ApiClient:
     def create_user(self, api_key, email):
         url = f"{self.base_url}/api-v2/users"
         headers = {
-        'Authorization': f'Bearer {api_key}',
-        'Content-Type': 'application/json'
-    }
+            'Authorization': f'Bearer {api_key}',
+            'Content-Type': 'application/json',
+        }
         resp = requests.post(url, headers=headers, json={
-        "email": email,
-        "isAdmin": False
-    })
-        resp.raise_for_status()
-        return resp.json()
+            "email": email,
+            "isAdmin": False,
+            }
+        )
+        return resp
 
     def create_project(self, api_key, title, users):
         url = f"{self.base_url}/api-v2/projects"
         headers = {
-        'Authorization': f'Bearer {api_key}',
-        'Content-Type': 'application/json'
-    }
-        body = {
-        "title": title,
-        "users": users
-    }
-        resp = requests.post(url, headers=headers, json=body)
-        resp.raise_for_status()
-        return resp.json()
+            'Authorization': f'Bearer {api_key}',
+            'Content-Type': 'application/json',
+        }
+        resp = requests.post(url, headers=headers, json={
+            "title": title,
+            "users": users,
+        })
+        return resp
 
     def edit_project(self, api_key, project_id, title, users, deleted=False):
         url = f"{self.base_url}/api-v2/projects/{project_id}"
         headers = {
             'Authorization': f'Bearer {api_key}',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         }
         resp = requests.put(url, headers=headers, json={
             "title": title,
             "users": users,
-            "deleted": deleted
+            "deleted": deleted,
         })
-        resp.raise_for_status()
-        return resp.json()
+        return resp
